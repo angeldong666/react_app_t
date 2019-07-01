73bc3b8eca77110901ecbfddbd5ef3cee3642d72
xiaoxd <1249621169@qq.com>
2019-07-01 11:17:51 +0800

    nor

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 
 class CommentInput extends Component {
-  render() {
-    return (
-      <div>CommentInput</div>
-    )
-  }
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            content: ''
+        }
+    }
+    
+    render() {
+        return (
+            <div className='comment-input'>
+                <div className='comment-field'>
+                    <span className='comment-field-name'>用户名：</span>
+                    <div className='comment-field-input'>
+                        <input />
+                    </div>
+                </div>
+                <div className='comment-field'>
+                    <span className='comment-field-name'>评论内容：</span>
+                    <div className='comment-field-input'>
+                        <textarea />
+                    </div>
+                </div>
+                <div className='comment-field-button'>
+                    <button>发布</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default CommentInput
